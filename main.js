4b2316574fe30eeadcc21a476c31396e434da574
ribeirooo2023 <130576298+ribeirooo2023@users.noreply.github.com>
2023-10-11 11:05:20 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert('não tem som');
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play('');
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom
+
+while(){
+    listaDeTeclas[0].onclick = tocasomPom;
+}
+
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick = tocaSomTim;
+listaDeTeclas[3].onclick = tocaSomPuff;

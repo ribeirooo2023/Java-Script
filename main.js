4fb23e301a025eee8249187d43cd2acd0dff1fbf
ribeirooo2023 <130576298+ribeirooo2023@users.noreply.github.com>
2023-11-01 10:58:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play('');
+function tocaSomPom(idElementoAudio){
+    document.querySelector(idElementoAudio).play('');
+}
+            if(elemento ===null){
+                
+            }
+if(elemento !=null && elemento.localname === 'audio'){
+    elemento.play(;)
+}else{
+    alert('elemento nao encontrado')
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
-
-while(contador < 9){
+for(contador = 0; contador <listaDeTeclas.length)
   
     const instrumento =listaDeTeclas [contador].classList
     listaDeTeclas[contador].onclick = function(){
     const tecla = listaDeTeclas[contador];
     }
-}  listaDeTeclas[contador].onclick = function(){
+        tecla.onclick = function(){
+            tocaSom(idAudio);
+        }
+        tocaSom('#som_tecla_clap');
+    tecla.onkeydow = function (evento){
+        if(evento.code === 'Space'|| evento.code === 'Enter')
+            tecla.classList.add('ativa');
+        }
+     
+        tecla.onkeyup = function(){
+            tecla.classList.remove('ativa');
+        }
     
-}
+
     
 
